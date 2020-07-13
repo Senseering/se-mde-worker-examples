756c97009621530c9f3f77d786c1e6cb78bbbadb
Felix Mönckemeyer <f.moenckemeyer@senseering.de>
2020-07-13 17:38:33 +0200

    iota test

--- source/index.js
+++ source/index.js
@@ -1,13 +1,24 @@
-let Worker = require('@senseering/worker')
+let Worker = require('../../worker_js')
 
 let config = './config.json';
 
 let worker = new Worker();
 
+let weights = [Math.random(), Math.random(), Math.random()];
+
+
 (async function () {
     await worker.connect(config)
-
-    let data = { test: 'Hello world!' }
-    await worker.publish(data)
-    await worker.disconnect()
+    let data = {
+        force: 2000,    // in kN
+        volume: 80,     // in dB
+        ppm: 20,        // parts per Minute
+    }
+    while(true){
+        data.force = Math.max(Math.min(((Math.random() * 2000) * (1 - weights[0])) + (data.force * weights[0]), 2500), 0)
+        data.volume = Math.max(Math.min(((Math.random() * 80) * (1 - weights[1])) + (data.volume * weights[1]), 80), 0)
+        data.ppm = Math.max(Math.min(((Math.random() * 20) * (1 - weights[2])) + (data.ppm * weights[2]), 40), 0)
+        await worker.publish(data)
+        await new Promise(resolve => setTimeout(resolve, 15000))
+    }
 })();
